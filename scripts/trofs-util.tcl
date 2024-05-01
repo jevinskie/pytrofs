f962e2a1cca3e8c8fdce5ea02431312f20663069
Jevin Sweval <jevinsweval@gmail.com>
2024-05-01 17:27:48 -0400

    trofs-util.tcl seems ok

--- scripts/trofs-util.tcl
+++ scripts/trofs-util.tcl
@@ -9,6 +9,7 @@ namespace import trofs::*
 set options {
     {c "create a trofs archive"}
     {x "extract a trofs archive"}
+    {f "force overwriting output path"}
     {i.arg "" "input directory or trofs archive"}
     {o.arg "" "output directory or trofs archive"}
 }
@@ -27,7 +28,44 @@ try {
     exit 1
 }
 
-puts "argv: $argv"
 parray params
 
-# archive hello_dmg_rootfs hello_dmg_rootfs.trofs
+if {([llength argv] != 0) && ($argv != "")} {
+    puts "Extra arguments '$argv' detected"
+    exit 1
+}
+if {$params(i) eq ""} {
+    puts "Must provide an input path with -i <path>"
+    exit 1
+} elseif {$params(o) eq ""} {
+    puts "Must provide an output path with -o <path>"
+    exit 1
+}
+if {!($params(c) ^ $params(x))} {
+    puts "Must specify one of -x or -c but not both"
+    exit 1
+}
+
+if {![file exists $params(i)]} {
+    if {$params(c) && ![file isdirectory $params(i)]} {
+        puts "When compressing, input path ('$params(i)') must be a directory"
+        exit 1
+    }
+}
+
+if {[file exists $params(o)] && !$params(f)} {
+    puts "Cowardly refusinig to overwrite output path '$params(o)'"
+} else {
+    file delete -force $params(o)
+}
+
+set abs_out [file normalize $params(o)]
+
+if {$params(c)} {
+    cd [file dirname $params(i)]
+    archive [file tail $params(i)] $abs_out
+} else {
+    set mounted_dir [mount $params(i)]
+    file copy $mounted_dir $abs_out
+    unmount $mounted_dir
+}
